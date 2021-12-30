7da1e444baded2d1385e2f91196b74d0302da825
apple-yagi <applesryu@gmail.com>
2021-12-30 20:51:16 +0900

    Update schema

--- prisma/migrations/20211230113224_init/migration.sql
+++ prisma/migrations/20211230113224_init/migration.sql
@@ -5,7 +5,6 @@ CREATE TABLE "Task" (
     "description" TEXT,
     "done" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
